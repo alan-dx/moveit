aea83bc59d900a2b215da7dacbdd118295223743
Alan Almeida <alanalmeida.emp@gmail.com>
2021-02-27 02:59:00 -0300

    finsih LevelUpModal | getServerSideProps | day 5 before deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import { GetServerSideProps } from 'next'
+
 import ChallengeBox from "../components/ChallengeBox";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import Countdown from "../components/Countdown";
@@ -7,28 +9,60 @@ import Profile from "../components/Profile";
 import { CountdownProvider } from "../contexts/CountdownContext";
 
 import styles from '../styles/pages/Home.module.css';
+import { ChallengesProvider } from "../contexts/ChallengesContext";
 
-export default function Home() {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props: HomeProps) {
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Início | MoveIt</title>
-      </Head>
+    <ChallengesProvider
+      //enviando informaçõe para o ChallengesContext
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Início | MoveIt</title>
+        </Head>
 
-     <ExperienceBar />
+        <ExperienceBar />
 
-    <CountdownProvider>
-      <section>
-        <div>
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-        </div>
-        <div>
-            <ChallengeBox />
-        </div>
-      </section>
-    </CountdownProvider>
+        <CountdownProvider>
+          <section>
+            <div>
+                <Profile />
+                <CompletedChallenges />
+                <Countdown />
+            </div>
+            <div>
+                <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
     </div>
+
+    </ChallengesProvider>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  // o código aqui colocado é executado no server do NextJs
+  // realiza chamadas assincronas antes mesmo da interface ser montada
+  // Lembre-se que a interface é montada após a execução do server NextJs
+
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
 }
